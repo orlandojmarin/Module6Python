b0082901b0cf9a20505934089c1072718103238a
orlandomarin <orlandomarin@orlandos-air.lan>
2024-06-05 23:37:54 -0400

    Sixth Python Program - Dictionaries

---
+++ main.py
@@ -0,0 +1,45 @@
+# Create a dictionary that maps the following countries to their respective 
+# capitals. The capitals are indicated in parentheses beside the country names 
+# below.
+# USA (Washington, D.C.)
+# United Kingdom (London)
+# China (Beijing)
+# Japan (Tokyo)
+# France (Paris)
+# The country name should serve as the key for accessing the capital. 
+# Next, print the dictionary.
+
+# create the country capitals dictionary
+country_capitals_dictionary = {"USA": "Washington, DC", "United Kingdom": 
+"London", "China": "Beijing", "Japan": "Tokyo", "France": "Paris"}
+
+# print the country capitals dictionary
+print(f"Hi Orlando! The dictionary is {country_capitals_dictionary}")
+
+# create a variable for the usa capital and print it
+usa_capital = country_capitals_dictionary["USA"]
+
+print(f"The capital of the USA is {usa_capital}")
+
+# create a variable for the uk capital and print it
+uk_capital = country_capitals_dictionary["United Kingdom"]
+
+print(f"The capital of the United Kingdom is {uk_capital}")
+
+# create a variable for the capital of china and print it
+china_capital = country_capitals_dictionary["China"]
+
+print(f"The capital of China is {china_capital}")
+
+# create a variable for the capital of japan and print it
+japan_capital = country_capitals_dictionary["Japan"]
+
+print(f"The capital of Japan is {japan_capital}")
+
+# create a variable for the capital of france and print it
+france_capital = country_capitals_dictionary["France"]
+
+print(f"The capital of France is {france_capital}")
+
+
+
